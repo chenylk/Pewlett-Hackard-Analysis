38956bb8051a2da23521e9d0df20e8606f3c5f22
Data Boot Camp <chenylk@outlook.com>
2020-08-30 22:50:46 -0400

    updated query code for employee retirement analysis

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -12,7 +12,7 @@ ON e.emp_no = t.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no ASC;
 
-select * from titles;
+select * from salaries;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no)
@@ -27,7 +27,7 @@ ORDER BY emp_no ASC, to_date DESC;
 --retrieve the number of titles from the unique titles table
 SELECT COUNT(title),
 title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
@@ -49,3 +49,27 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no ASC;
+
+SELECT COUNT(emp_no)
+FROM mentorship_eligibility;
+
+SELECT COUNT(emp_no)
+FROM unique_titles;
+
+SELECT SUM(s.salary),
+	ut.title
+FROM unique_titles as ut
+LEFT JOIN salaries as s
+ON (ut.emp_no = s.emp_no)
+GROUP BY ut.title;
+
+SELECT COUNT(ut.emp_no),
+	d.dept_name
+FROM unique_titles as ut
+LEFT JOIN dept_emp as de
+ON (ut.emp_no = de.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+GROUP BY d.dept_name; 
+
+
